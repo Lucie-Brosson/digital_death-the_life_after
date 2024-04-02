434ee63dee08b0eea9bc4390a15d296e10af283c
Lucie-Maya <luciemayabrosson@gmail.com>
2024-04-02 12:25:06 +0200

    modif

--- Jeux/Love/Love.js
+++ Jeux/Love/Love.js
@@ -1,3 +1,34 @@
-document.getElementById("demo").innerHTML = "Hello World"
+console.log("Start");
 
-console.log("Hzlo")
+///////////////////////////////////////////////////////////////////
+////// library 
+//////////////////////////////////////////////////////////////////
+
+//import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm"
+
+///////////////////////////////////////////////////////////////////
+////// Variable 
+//////////////////////////////////////////////////////////////////
+
+var question_stage = 0
+
+//document.getElementById("question").innerHTML = "Hello Dolly.";
+
+document.getElementById("demo").innerHTML="Paragraph chdddddddddddddanged.";
+
+///////////////////////////////////////////////////////////////////
+////// Question 
+//////////////////////////////////////////////////////////////////
+
+function myFunction() {
+    console.log("game started")
+    document.getElementById("question").innerHTML="Paragraphhhhhhhhhh changed.";
+
+    // Change answer 
+
+    document.getElementById("answer1").innerHTML="Paragraphhhhhhhhhh changed.";
+    document.getElementById("answer2").innerHTML="Paragraphhhhhhhhhh changed.";
+    document.getElementById("answer3").innerHTML="Paragraphhhhhhhhhh changed.";
+    document.getElementById("answer4").innerHTML="Paragraphhhhhhhhhh changed.";
+
+}
